814018b230b94b89028c2755d7c556a0db3917dc
naveen rawat <naveen.rawat@protonshub.in>
2024-11-29 10:39:03 +0530

    refactor spec

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,7 +1,6 @@
 RSpec.describe StringCalculator do
-  describe 'Add' do
-    it 'returns 0 for an empty string' do
-      expect(StringCalculator.add('')).to eq(0)
-    end
+  it "returns 0 for an empty string" do
+    calculator = StringCalculator.new
+    expect(calculator.add("")).to eq(0)
   end
 end
